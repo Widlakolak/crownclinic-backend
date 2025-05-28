0f0aeaea63f11e49cbdf8937acaf846bb69ca4e8
Peter <piowiedlocha@gmail.com>
2025-05-28 21:43:24 +0200

    first commit

---
+++ src/main/java/com/crown/backend/CrownclinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crown.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownclinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrownclinicBackendApplication.class, args);
+	}
+
+}
